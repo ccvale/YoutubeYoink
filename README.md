ea14c7346776fb69e9c29679e6483aa30f4ac1ab
Chris Valente <63901165+ccvale@users.noreply.github.com>
2023-01-01 01:05:33 -0500

    lowkey done

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 # YoutubeYoink
 
-quick GUI tool to rip mp3/mp4 files from youtube videos; I'm sick of going to shady sites to do it, so here we are.
+## Because who the hell wants to keep going to shady sites?
+
+A quick GUI tool made for fun/my individual use. Written in Python with `CustomTKinter` to rip either MP3 (raw audio) or MP4 (the full video content) files from YouTube videos.
+
+## Installation
+
+I figured that since I created an executable I would also include a way to download the `.exe` that was created for this project, since I made one to use:
+
+```bash
+  see somewhere...
+```
+
+## Run Locally
+
+If you want to check out the project, but don't want to download the executable, feel free to do the following:
+
+- Clone the project
+
+```bash
+  git clone https://github.com/ccvale/YoutubeYoink.git
+```
+
+- Go to the project directory
+
+```bash
+  cd YoutubeYoink
+```
+
+- Install the required packages
+
+```bash
+  pip install -r requirements.txt
+```
+
+- Run the program
+
+```bash
+  python app.py
+```
--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 import customtkinter as tk
+from PIL import Image, ImageTk
 from YoutubeVideo import YoutubeVideo
 
-# not used; customtkinter doesnt like these colors for some reason
-class PASTEL_ENUM:
-    PINK = '#df8598',
-    PURPLE = '#aa93dd',
+class ENUM:
+    PINK = '#df8598'
+    DARK_PINK = '#b26a79'
+    DARKER_PINK = '#9c5d6a'
+    PURPLE = '#aa93dd'
     BLUE = '#afcbff'
+    DEFAULT_GREEN = '#54a376'
+    RED = '#d22b2b'
+    FONT = '<MesloLGS NF>'
+    COMP = '#8598df'
 
 class App(tk.CTk):
     def __init__(self):
         super().__init__()
 
         tk.set_appearance_mode('system')
-        tk.set_default_color_theme('green')
 
         self.title('Youtube Yoink')
         self.geometry('600x450')
         self.resizable(False, False)
+
+        ico = Image.open('star.ico')
+        photo = ImageTk.PhotoImage(ico)
+        self.wm_iconphoto(False, photo)
 
         self.buttonText = tk.StringVar(value='Convert')
 
@@ -25,43 +34,55 @@ class App(tk.CTk):
         self.frame.pack(pady=20, padx=60, fill='both', expand=True)
 
         # title heading
-        self.label = tk.CTkLabel(master=self.frame, text='Youtube Yoink')
-        self.label.pack(pady=12, padx=10)
+        self.title = tk.CTkLabel(master=self.frame, text='Youtube Yoink', font=(ENUM.FONT, 30))
+        self.title.pack(pady=12, padx=10)
 
         # URL text heading
-        self.label = tk.CTkLabel(master=self.frame, text='URL')
-        self.label.place(relx=0.155, rely=0.155)
+        self.label = tk.CTkLabel(master=self.frame, text='URL', font=(ENUM.FONT, 20))
+        self.label.place(relx=0.155, rely=0.175)
 
         # link entry field
         self.entry = tk.CTkTextbox(master=self.frame, width=250, height=3, activate_scrollbars=False)
         self.entry.pack(pady=12, padx=10)
 
-        self.combobox = tk.CTkOptionMenu(master=self.frame, values=['Select an option:', 'MP3', 'MP4'])
+        self.combobox = tk.CTkOptionMenu(master=self.frame, values=['MP3', 'MP4'], fg_color=ENUM.PINK, button_color=ENUM.DARK_PINK, button_hover_color=ENUM.DARKER_PINK, font=(ENUM.FONT, 15))
         self.combobox.pack(padx=20, pady=10)
         self.combobox.set('Select an option:')
 
         # convert button
-        self.button = tk.CTkButton(master=self.frame, text='Convert', command=self.execute, textvariable=self.buttonText)
+        self.button = tk.CTkButton(master=self.frame, text='Convert', command=self.execute, textvariable=self.buttonText, fg_color=ENUM.PINK, hover_color=ENUM.DARK_PINK, font=(ENUM.FONT, 15))
         self.button.pack(pady=12, padx=10)
     
     def execute(self):
         self.buttonText.set('Convert')
-        #self.button.configure(fg_color='green')
+        self.button.configure(fg_color=ENUM.PINK)
+
+        url = self.entry.get('0.0', 'end')
+        choice = self.combobox.get()
+        self.update()
+        
         try:
-            url = self.entry.get('0.0', 'end')
-            choice = self.combobox.get()
+            # handling on submit
             self.buttonText.set('Downloading...')
             self.button.state = 'disabled'
+            self.update()
 
+            # these will reset no matter what
             self.entry.delete('0.0', 'end')
-            self.combobox.set('Select an option')
+            self.combobox.set('Select an option:')
 
             YoutubeVideo(url, choice).convert()
+
+            # handling the reset
             self.button.state = 'normal'
             self.buttonText.set('Convert')
+            self.update()
+
         except:
             self.buttonText.set('Try Again')
-            self.button.configure(fg_color='#D22B2B')
+            self.button.configure(fg_color=ENUM.COMP)
+            self.update()
+
 
 if __name__ == '__main__':
     app = App()
